e92ce3151798eee97fba0d4bc367cd4ab8cf5b9f
AlexisNguemby <alexis.nguemby-mbina@laplateforme.io>
2026-01-27 18:57:09 +0100

    Modification of cards model + adding few cards as test

--- src/models/Card.js
+++ src/models/Card.js
@@ -12,18 +12,14 @@
 
 
 class Card {
-    constructor(name, faction, power, attack,){
+    constructor(name, faction, power, attack,description,image = "") {
         this.name = name;
         this.faction= faction;
         this.power = power;
         this.attack = attack;
+        this.description = description;
+        this.image = image; // ex: "assets/cards/human_001.png"
 
     }
-
-
-
-
-
-
 
 }
